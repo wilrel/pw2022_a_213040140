f347608104abc3a1914765219e053c519c74d0e2
wilrel <wildannasrulloh321@gmail.com>
2022-06-03 23:23:19 +0700

    Login

--- tubes/tukangpc/adminpanel/index copy.php
+++ tubes/tukangpc/adminpanel/index copy.php
@@ -1,7 +1,3 @@
-<?php
-require "session.php";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,11 +9,18 @@ require "session.php";
   <script src="https://kit.fontawesome.com/37eb096f3a.js" crossorigin="anonymous"></script>
   <title>Document</title>
 </head>
-
-
+<style>
+  body {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
 
 <body>
-  <h2>Halo Admin</h2>
+  <i class="fas fa-shopping-cart"></i>
+  <i class="fa-solid fa-gamepad"></i>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 
--- tubes/tukangpc/adminpanel/login.php
+++ tubes/tukangpc/adminpanel/login.php
@@ -11,9 +11,39 @@ require "../koneksi.php";
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
+<style>
+  .main {
+    height: 100vh;
+  }
+
+  .login-box {
+    width: 600px;
+    height: 400px;
+    border: solid 5px;
+  }
+</style>
 
 <body>
+  <div class="main d-flex justify-content-center align-items-center">
+    <form action="" method="POST">
+      <div class="mb-3">
+        <label for="exampleInputUsername" class="form-label">Username</label>
+        <input type="text" class="form-control" id="exampleInputUsername" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">Silahkan Isi Username Anda</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
 
 </body>
 
