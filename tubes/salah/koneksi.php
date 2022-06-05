5295adc22b5ee25678809d4c00fff15120783f41
wilrel <wildannasrulloh321@gmail.com>
2022-06-05 12:59:24 +0700

    Memperbaiki kesalahan

--- tubes/salah/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  $host = 'localhost';
-  $username = 'root';
-  $password = '';
-  $db = 'tukangpc';
-
-  $conn = mysqli_connect($host, $username, $password, $db) or die ('Gagal Terhubung Dengan Database Yang Dituju');
